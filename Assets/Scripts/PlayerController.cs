174211c7be8bcc4ffe631dff51bc1f79412a310b
Dinara <inraidi@gmail.com>
2020-01-31 13:40:26 -0500

    Added Vertical motion and invisible borders for movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float minX, maxX, minY, maxY;
+    private Rigidbody2D rBody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         float horiz = Input.GetAxis("Horizontal");
-        GetComponent<Rigidbody2D>().velocity = new Vector2(horiz, 0.0f);
+        float vert = Input.GetAxis("Vertical");
+
+       rBody.velocity = new Vector2(horiz, vert) * speed;
+        rBody.position = new Vector2(
+            Mathf.Clamp(rBody.position.x, minX, maxX),
+            Mathf.Clamp(rBody.position.y, minY, maxY)
+            );
 
 
 
